afabd23dce06aa51c7233d9f4c826117de9dc958
Santosh Phaiju <santoshphaiju062@gmail.com>
2022-06-06 13:11:49 +0545

    Another commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Alert from "./components/Alert";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
